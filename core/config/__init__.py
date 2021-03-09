0a10c3d2ab3a133ecc9c6f49e35b3ffab719682c
Osipov Andrey <osyapov@bk.ru>
2021-03-09 19:06:22 +0300

    Fixed README

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -1,11 +1,4 @@
 import os
-# import pathlib
-# import yaml
-
-
-# root = pathlib.Path(__file__).parent.absolute()
-# with open(root.joinpath("config.yaml"), "r") as f:
-#     config = yaml.load(f, Loader=yaml.SafeLoader)
 
 
 config = dict(
