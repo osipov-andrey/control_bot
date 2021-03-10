9986e7d2bdece3d9e393936fa5997925fb51a886
aosipov <aosipov@vertex.spb.ru>
2021-03-09 22:23:56 -0800

    Alembic in Docker

--- core/repository/alembic/env.py
+++ core/repository/alembic/env.py
@@ -17,6 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
 from core.repository import _schema as models
 
 target_metadata = models.metadata
