71a5804cb33dd9a8821e994c8c2793bd41324550
andreyosipov <Andrey.A.Osipov@nordigy.ru>
2021-05-04 19:27:26 +0300

    Hotfix in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,23 +1,22 @@
 #! /bin/sh
 
-cd core/repository
+cd core/repository || exit
 ../../venv/bin/alembic upgrade head
 cd ../..
 
 rabbit=0
-for attempt in 1 2 3
-	do
-	if $( ! nc -zvw3 "${RABBIT_HOST}" "${RABBIT_PORT}" ); then 
-		sleep 3
-	else 
-		echo "Connected to RabbitMQ"
-		rabbit=1
-		break
-        fi
-        done
+for _ in 1 2 3; do
+  if ! nc -zvw3 "${RABBIT_HOST}" "${RABBIT_PORT}"; then
+    sleep 10
+  else
+    echo "Connected to RabbitMQ"
+    rabbit=1
+    break
+  fi
+done
 if [ $rabbit -eq 0 ]; then
-	echo "Can not connect to RabbitMQ!"
-	exit 1
+  echo "Can not connect to RabbitMQ!"
+  exit 1
 fi
 
 ./venv/bin/python3 -m core run
