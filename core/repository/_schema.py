a4e169d2c5fd9cce1b3c94297a836267a57096c4
andreyosipov <Andrey.A.Osipov@nordigy.ru>
2021-07-24 16:05:04 +0300

    Rewrited models to the declarative base (ORM)

--- core/repository/_schema.py
+++ core/repository/_schema.py
@@ -1,93 +1,67 @@
-from collections import namedtuple
-from typing import Type
-
-from sqlalchemy import Table, Column, MetaData, Boolean, Integer, String
+from sqlalchemy import Column, Boolean, Integer, String
 from sqlalchemy import ForeignKey, PrimaryKeyConstraint, UniqueConstraint
+from sqlalchemy.orm import declarative_base
 
 
-def _create_mapping(table: Table) -> Type[tuple]:
-    fields = table.c.keys()
-    name: str = _to_pascal_case(table.name)
-    mapping = namedtuple(name, fields)
-    return mapping
+Base = declarative_base()
 
 
-def _to_pascal_case(snake_case: str) -> str:
-    parts = snake_case.split("_")
-    pascal = "".join(part.capitalize() for part in parts)
-    return pascal
+class UserChannel(Base):
+    """Many-to-Many table for Users/Channel subscribing"""
+    __tablename__ = "user_channel"
 
-
-metadata = MetaData()
-
-
-channels_users_associations = Table(
-    "user_channel",
-    metadata,
-    Column(
-        "user_id",
+    user_id: int = Column(
         Integer,
         ForeignKey("user.id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
-    ),
-    Column(
-        "channel_id",
+    )
+    channel_id: int = Column(
         ForeignKey("channel.id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
-    ),
-    PrimaryKeyConstraint("user_id", "channel_id", name="user_channel_pk"),
-)
-UserChannel = _create_mapping(channels_users_associations)
+    )
+    user_channel_pk = PrimaryKeyConstraint("user_id", "channel_id", name="user_channel_pk")
 
 
-actuators_users_associations = Table(
-    "user_actuator",
-    metadata,
-    Column(
-        "user_id",
+class UserActuator(Base):
+    """Many-to-Many table for Users/Actuator revoking"""
+    __tablename__ = "user_actuator"
+
+    user_id: int = Column(
         Integer,
         ForeignKey("user.id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
-    ),
-    Column(
-        "actuator_id",
+    )
+    actuator_id: int = Column(
         Integer,
         ForeignKey("actuator.id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
-    ),
-    PrimaryKeyConstraint("user_id", "actuator_id", name="user_actuator_pk"),
-)
-UserActuator = _create_mapping(actuators_users_associations)
-
-users_table = Table(
-    "user",
-    metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True, index=True),
-    Column("telegram_id", Integer, unique=True, index=True, nullable=False),
-    Column("telegram_username", String, unique=True, nullable=False),
-    Column("name", String, nullable=True),
-    Column("phone_number", String, nullable=True),
-    Column("is_admin", Boolean, default=False),
-    UniqueConstraint("telegram_id", "telegram_username", name="uq_id_user"),
-)
-User = _create_mapping(users_table)
+    )
+    user_actuator_pk = PrimaryKeyConstraint("user_id", "actuator_id", name="user_actuator_pk")
 
 
-channel_table = Table(
-    "channel",
-    metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True, index=True),
-    Column("name", String, unique=True, nullable=False, index=True),
-    Column("description", String, nullable=True),
-)
-Channel = _create_mapping(channel_table)
+class User(Base):
+    __tablename__ = "user"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    telegram_id: int = Column(Integer, unique=True, index=True, nullable=False)
+    telegram_username: str = Column(String, unique=True, nullable=False)
+    name: str = Column(String, nullable=True)
+    phone_number: str = Column(String, nullable=True)
+    is_admin: bool = Column(Boolean, default=False)
+    uq_id_user = UniqueConstraint("telegram_id", "telegram_username", name="uq_id_user")
 
 
-actuators_table = Table(
-    "actuator",
-    metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True, index=True),
-    Column("name", String, unique=True, nullable=False, index=True),
-    Column("description", String, nullable=True),
-)
-Actuator = _create_mapping(actuators_table)
+class Channel(Base):
+    __tablename__ = "channel"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name: str = Column(String, unique=True, nullable=False, index=True)
+    description: str = Column(String, nullable=True)
+
+
+class Actuator(Base):
+    __tablename__ = "actuator"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name: str = Column(String, unique=True, nullable=False, index=True)
+    description: str = Column(String, nullable=True)
