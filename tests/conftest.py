da2a6a8dd84750697bfcd77a07ab505d7374963d
andreyosipov <Andrey.A.Osipov@nordigy.ru>
2021-09-06 23:13:28 +0300

    Added test channels to the test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import List
 
 import pytest
-from core.repository import User, Base
+from core.repository import User, Base, Channel
 
 from faker import Faker
 from sqlalchemy import create_engine
@@ -19,6 +19,15 @@ users = [
             telegram_username=fake.user_name(),
             name=fake.name(),
             is_admin=False,
+        )
+    )
+    for _ in range(10)
+]
+
+channels = [
+    Channel(
+        **dict(
+            id=fake.unique.random_int(), name=fake.domain_word(), description=fake.catch_phrase()
         )
     )
     for _ in range(5)
@@ -39,6 +48,7 @@ def test_database() -> Session:
     session = Session()
 
     _add_users(session, users)
+    _add_channels(session, channels)
 
     yield session
     session.close()
@@ -47,3 +57,7 @@ def test_database() -> Session:
 
 def _add_users(session: Session, users_: List[User]) -> None:
     session.add_all(users_)
+
+
+def _add_channels(session: Session, channels_: List[Channel]) -> None:
+    session.add_all(channels_)
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,24 +1,33 @@
 import random
 from typing import List
 
-from core.repository import Repository, User
+from core.repository import Repository, User, Channel
 from core.repository.repository import first
 from sqlalchemy.engine import Row
 from sqlalchemy.future import select
 
 
-from tests.conftest import PATH_TO_DB, Session, users
+from tests.conftest import PATH_TO_DB, Session, users, channels
 
 repository = Repository(path_to_db=PATH_TO_DB)
 
 
-def test_add_users(test_database: Session):
-    checking_user: User = random.choice(users)
-    user_result: Row[User] = test_database.execute(
-        select(User).where(User.id == checking_user.id)
-    ).first()
-    user: User = first(user_result)
-    assert user.id == checking_user.id
+class TestTestDatabase:
+    def test_add_users(self, test_database: Session):
+        checking_user: User = random.choice(users)
+        user_result: Row[User] = test_database.execute(
+            select(User).where(User.id == checking_user.id)
+        ).first()
+        user: User = first(user_result)
+        assert user.id == checking_user.id
+
+    def test_add_channels(self, test_database: Session):
+        checking_channel: Channel = random.choice(channels)
+        channel_result: Row[Channel] = test_database.execute(
+            select(Channel).where(Channel.id == checking_channel.id)
+        ).first()
+        channel: Channel = first(channel_result)
+        assert channel.id == checking_channel.id
 
 
 def insert_users(users_: List[User]) -> None:
